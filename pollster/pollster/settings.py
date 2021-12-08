a38866dda84753c5b49f50878be5781f17f1cf62
AthulSamuel7 <athulsamuel@gmail.com>
2021-12-08 10:26:29 +0530

    Django Pollster

--- pollster/pollster/settings.py
+++ pollster/pollster/settings.py
@@ -74,12 +74,36 @@ WSGI_APPLICATION = 'pollster.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'library',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
+# DATABASES = {  
+#     'default': {  
+#         'ENGINE': 'django.db.backends.mysql',  
+#         # 'NAME': 'djangoApp',  
+#         'NAME': os.path.join(BASE_DIR, 'db.mysql'),
+#         'USER':'root',  
+#         'PASSWORD':'1234',  
+#         'HOST':'localhost',  
+#         'PORT':'3306'  
+#     }  
+# } 
 
 
 # Password validation
